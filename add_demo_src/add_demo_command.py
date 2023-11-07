e9d91fd6e223c3e5f9c96c53c90fc3c91ddb6014
rachelmsimm <rachel.simm@unity3d.com>
2023-11-06 17:20:09 -0800

    add more descriptive help text

--- add_demo_src/add_demo_command.py
+++ add_demo_src/add_demo_command.py
@@ -3,23 +3,21 @@ from demo import Demo
 
 import argparse
 
-# TODO add help text
 class AddDemoCommand(object):
     def execute(self):
-        parser = argparse.ArgumentParser(description="Add a demo to the list of demos.", 
-                                         usage="TODO")
-        parser.add_argument("name", help="The name of the demo.")
-        parser.add_argument("key", help="The key of the demo.")
-        parser.add_argument("url", help="The url of the demo.")
-        parser.add_argument("--size", help="The size of the demo.", default=157.6*1024*1024)
-        parser.add_argument("--heap", help="The heap of the demo.", default=387*1024*1024)
-        parser.add_argument("--compiler", help="The compiler of the demo.", default="")
-        parser.add_argument("--engine", help="The engine of the demo.", default="")
-        parser.add_argument("--date", help="The date of the demo.", default="")
-        parser.add_argument("--apis", help="The apis of the demo.", default=["wasm", "URP", "WebGL 2", "linear-color-space"])
-        parser.add_argument("--noVsync", help="The noVsync of the demo.", default=True)
-        parser.add_argument("--interactive", help="The interactive of the demo.", default=True)
-        parser.add_argument("--mobile", help="The mobile of the demo.", default=True)
+        parser = argparse.ArgumentParser(description="Add a demo to the list of demos to be run by Emunittest")
+        parser.add_argument("name", help="The name of the demo (must be unique)")
+        parser.add_argument("key", help="The key of the demo (must be unique)")
+        parser.add_argument("url", help="The path to the demo, usually to its index.html page, relative to the root of Emunittest")
+        parser.add_argument("--size", help="The disk size of the demo in MB", default=157.6*1024*1024)
+        parser.add_argument("--heap", help="The heap size of the demo in MB", default=387*1024*1024)
+        parser.add_argument("--compiler", help="The compiler used to compile the build", default="")
+        parser.add_argument("--engine", help="The version of Unity that was used", default="")
+        parser.add_argument("--date", help="The date the demo was created or added", default="")
+        parser.add_argument("--apis", help="The APIs used by the build/demo", default=["wasm", "URP", "WebGL 2", "linear-color-space"])
+        parser.add_argument("--noVsync", help="Whether or not no Vsync is used", default=True)
+        parser.add_argument("--interactive", help="Whether or not the demo is interactive (interactive demos have inputstream.js files)", default=True)
+        parser.add_argument("--mobile", help="Whether the demo/build is meant to be run on mobile or not", default=True)
 
         args = parser.parse_args()
 
