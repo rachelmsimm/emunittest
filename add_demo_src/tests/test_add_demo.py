90de5c1800a8c8365a2109da100db6510c4fa438
rachelmsimm <rachel.simm@unity3d.com>
2023-11-08 15:40:20 -0800

    update mock function name to be consistent with real function name

--- add_demo_src/tests/test_add_demo.py
+++ add_demo_src/tests/test_add_demo.py
@@ -20,10 +20,10 @@ mock_demo_obj = Demo("game1", "game1", "demos/game1/index.html")
 
 class TestAddDemo:
     def test_add_demo_success(mocker):
-        with patch("add_demo_src.add_demo.insert_demo", return_value=None) as mock_insert_entry:
+        with patch("add_demo_src.add_demo.insert_demo", return_value=None) as mock_insert_demo:
             with patch("add_demo_src.add_demo.get_existing_demos", return_value={}):
                 add(mock_demo_obj)
-                assert(mock_insert_entry.call_count == 1)
+                assert(mock_insert_demo.call_count == 1)
 
     def test_add_demo_duplicate_name_or_key(mocker):
         with patch("add_demo_src.add_demo.get_existing_demos", return_value=mock_demo_list):
