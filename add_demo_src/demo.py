3a04f111f9c758991fb7cf9ab593fed8918b5653
rachelmsimm <rachel.simm@unity3d.com>
2023-11-09 11:39:06 -0800

    remove whitespace

--- add_demo_src/demo.py
+++ add_demo_src/demo.py
@@ -1,16 +1,16 @@
 class Demo:
-    def __init__(self, 
-                 name, 
-                 key, 
-                 url, 
-                 size=157.6*1024*1024, 
-                 heap=387*1024*1024, 
-                 compiler="", 
-                 engine="", 
-                 date="", 
-                 apis=["wasm", "URP", "WebGL 2", "linear-color-space"], 
-                 noVsync=True, 
-                 interactive=True, 
+    def __init__(self,
+                 name,
+                 key,
+                 url,
+                 size=157.6*1024*1024,
+                 heap=387*1024*1024,
+                 compiler="",
+                 engine="",
+                 date="",
+                 apis=["wasm", "URP", "WebGL 2", "linear-color-space"],
+                 noVsync=True,
+                 interactive=True,
                  mobile=True):
         self.name = name
         self.key = key
